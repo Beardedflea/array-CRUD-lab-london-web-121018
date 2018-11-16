0e97e21e72b14370b5d087ba65a13aa08743a7b6
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-11-16 17:49:49 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,15 +3,15 @@ def create_an_empty_array
 end
 
 def create_an_array
-  example_arrary= [1,2,3,4]
+  example_array= [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  example_array << 5
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  example_array.unshift 0
 end
 
 def remove_element_from_end_of_array(array)
